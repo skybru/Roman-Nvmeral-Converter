2aeb1b99a5fa7d1363e730815ce998dad7408c20
skybru <giorgio.bruschi98@gmail.com>
2024-07-28 16:49:04 +0200

    completed script with recursion

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const inputNumber = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("output");
+const valInt = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const valRoman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 
-const convertToRoman = (input) => {
-   if (input <= 0) {
-    result.textContent = "Please enter a number greater than or equal to 1";
-   } else if (input > 3999) {
-    result.textContent = "Please enter a number less than or equal to 3999";
-   }
-   console.log(Math.floor(input / 1000));
-   console.log(input % 1000);
+const convertToRoman = (input, index) => {
+    if (input <= 0) {
+        console.log("base");
+        return "";
+    }
 
-
+    for (let i = index; i < valInt.length; i++) {
+        if (input >= valInt[i]) {
+            console.log(valRoman[i]);
+            result.textContent += valRoman[i];
+            return convertToRoman(input - valInt[i], i)
+        }
+    }
 }
 
 const checkInput = () => {
     const inputParsed = parseInt(inputNumber.value);
-    
+
     if (isNaN(inputParsed)) {
         result.textContent = "Please enter a valid number";
+    } else if (inputParsed <= 0 ) {
+        result.textContent = "Please enter a number greater than or equal to 1";
+        return;
+    } else if (inputParsed > 3999) {
+        result.textContent = "Please enter a number less than or equal to 3999";
+        return;
     } else {
-        result.textContent = inputParsed; //logging
-        convertToRoman(inputParsed);
+        result.textContent = "";
+        convertToRoman(inputParsed, 0);
     }
 
+    inputNumber.value = "";
 }
 
 convertBtn.addEventListener("click", checkInput);
